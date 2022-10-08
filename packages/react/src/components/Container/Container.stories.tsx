69b9d8d1fdc7eb4bc5048119035bb70564111da3
Nishchith.Rao <nischitrao5@gmail.com>
2022-10-08 23:53:35 +0530

    feat(react-slider): create a container to init slider actions

---
+++ packages/react/src/components/Container/Container.stories.tsx
@@ -0,0 +1,40 @@
+import React from "react";
+import Container, { ContainerProps } from "./Container";
+
+export default {
+  title: "components/Container",
+  component: Container,
+};
+
+const Slide = (props: {
+  color: string;
+  children: React.HTMLAttributes<HTMLDivElement>["children"];
+}) => (
+  <div
+    style={{
+      height: "300px",
+      color: "white",
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "center",
+      background: props.color,
+      marginInline: "2px",
+      fontSize: "34px",
+    }}
+  >
+    {props.children}
+  </div>
+);
+
+export const basic = (args: ContainerProps) => (
+  <Container {...args}>
+    <Slide color="red">Slide 1</Slide>
+    <Slide color="green">Slide 2</Slide>
+    <Slide color="blue">Slide 3</Slide>
+    <Slide color="black">Slide 4</Slide>
+    <Slide color="brown">Slide 5</Slide>
+  </Container>
+);
+basic.args = {
+  transition: "default",
+} as ContainerProps;
---
+++ packages/react/src/components/Container/Container.tsx
@@ -0,0 +1,38 @@
+import React, { useCallback, useState } from "react";
+import "./Container.scss";
+import { SliderService, transitions } from "@slider/core";
+
+export interface ContainerProps {
+  children: React.HTMLAttributes<HTMLDivElement>["children"];
+  transition: keyof typeof transitions;
+}
+
+const Container: React.FC<ContainerProps> = (props: ContainerProps) => {
+  const { children, transition } = props;
+  const [containerEl, setContainerEl] = useState<HTMLDivElement>();
+
+  const containerRef = useCallback((ref: HTMLDivElement) => {
+    setContainerEl(ref);
+  }, []);
+
+  const service = new SliderService(containerEl, transitions[transition]);
+
+  return (
+    <div className="Container" ref={containerRef}>
+      {React.Children.map(children, (child, i) => {
+        return (
+          <div
+            data-index={i}
+            onTouchStart={service.handleTouchStart}
+            onTouchMove={service.handleTouchMove}
+            onTouchEnd={service.handleTouchEnd}
+          >
+            {child}
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+export default Container;
