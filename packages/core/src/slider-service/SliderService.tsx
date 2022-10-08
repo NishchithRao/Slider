6a5f4b31c7b6124f65754333bc2fd4733be5265b
Nishchith.Rao <nischitrao5@gmail.com>
2022-10-08 23:27:58 +0530

    feat(slider): create a service for sliders for touch events

---
+++ packages/core/src/slider-service/SliderService.tsx
@@ -0,0 +1,69 @@
+import { TouchEvent as ReactTouchEvent } from "react";
+import { getIndexNumber, moveToSlide } from "../utils/touch";
+
+export type TransitonFn = (
+  scrolledValue: number,
+  totalWidth: number,
+  elWidth: number,
+  touchDistance: number
+) => {
+  type: string;
+  value: string;
+  affectsOtherSlides?: boolean;
+}[];
+
+class SliderService {
+  static container: HTMLDivElement | null;
+  static store: number;
+  static transitonFn: TransitonFn;
+
+  constructor(container: HTMLDivElement | null, transitionFn: TransitonFn) {
+    SliderService.container = container;
+    SliderService.transitonFn = transitionFn;
+  }
+
+  public handleTouchEnd(ev: TouchEvent | ReactTouchEvent<HTMLDivElement>) {
+    const index = getIndexNumber(ev);
+    const touchDistance = SliderService.store - ev.changedTouches[0].clientX;
+    const touchDistancePercent =
+      (100 / (ev.currentTarget as HTMLDivElement).offsetWidth) *
+      Math.abs(touchDistance);
+    const isAllowed = touchDistance > 0 || index > 0;
+
+    if (touchDistancePercent > 30 && isAllowed) {
+      moveToSlide(
+        SliderService.container,
+        SliderService.transitonFn,
+        touchDistance > 0 ? index + 1 : index - 1,
+        0,
+        true
+      );
+    } else {
+      moveToSlide(
+        SliderService.container,
+        SliderService.transitonFn,
+        index,
+        0,
+        true
+      );
+    }
+  }
+
+  public handleTouchMove(ev: TouchEvent | React.TouchEvent<HTMLDivElement>) {
+    const currentTouchValue = ev.changedTouches[0].clientX;
+    const index = getIndexNumber(ev);
+    const touchDistance = SliderService.store - currentTouchValue;
+    moveToSlide(
+      SliderService.container,
+      SliderService.transitonFn,
+      index,
+      touchDistance
+    );
+  }
+
+  public handleTouchStart(ev: TouchEvent | React.TouchEvent<HTMLDivElement>) {
+    SliderService.store = ev.changedTouches[0].clientX;
+  }
+}
+
+export default SliderService;
---
+++ packages/core/src/slider-service/index.tsx
@@ -0,0 +1,1 @@
+export { default as SliderService, TransitonFn } from "./SliderService";
