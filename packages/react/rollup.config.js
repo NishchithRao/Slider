5ac90f0f2a7dbbb668cd37736325ae51cfc1d96d
Nishchith.Rao <nischitrao5@gmail.com>
2022-10-09 01:26:35 +0530

    chore(build): update build file paths + add plugins

--- packages/react/rollup.config.js
+++ packages/react/rollup.config.js
@@ -1,19 +1,20 @@
 const commonConfig = require("../../rollup.config");
 const pkg = require("./package.json");
 
-export default {
+module.exports = {
   ...commonConfig,
   input: "./src/index.ts",
   output: [
-    {
-      file: pkg.main,
-      format: "cjs",
-      exports: "named",
-    },
     {
       file: pkg.module,
       format: "esm",
       exports: "named",
     },
+    {
+      file: pkg.main,
+      format: "cjs",
+      exports: "named",
+    },
   ],
+  external: ["react", "react-dom"],
 };
--- rollup.config.js
+++ rollup.config.js
@@ -1,23 +1,19 @@
 const { nodeResolve } = require("@rollup/plugin-node-resolve");
 const commonjs = require("@rollup/plugin-commonjs");
-const typescript = require("@rollup/plugin-typescript");
+const typescript = require("rollup-plugin-typescript2");
 const del = require("rollup-plugin-delete");
+const postcss = require("rollup-plugin-postcss");
 
 module.exports = {
-  input: "./src/index.ts",
-  output: {
-    dir: "dist",
-    format: "cjs",
-  },
   plugins: [
-    nodeResolve(),
-    del({ targets: "dist" }),
-    commonjs(),
     typescript({
       tsconfig: "./tsconfig.json",
       declaration: true,
       declarationDir: "dist",
     }),
+    nodeResolve(),
+    del({ targets: ["dist"], verbose: true, hook: "buildStart" }),
+    commonjs(),
+    postcss(),
   ],
-  external: ["react", "react-dom"],
 };
