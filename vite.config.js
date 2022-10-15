0ce52a416349cc471f40014f76942102a7cb96b2
Nishchith.Rao <nischitrao5@gmail.com>
2022-10-16 01:36:05 +0530

    chore(build): create shared vite config

---
+++ vite.config.js
@@ -0,0 +1,28 @@
+import dts from 'vite-plugin-dts';
+import { visualizer } from 'rollup-plugin-visualizer';
+
+const sharedConfig = (pkgJSON) => ({
+  plugins: [
+    dts({
+      tsConfigFilePath: './tsconfig.json'
+    }),
+    visualizer({
+      filename: 'dist/stats.html'
+    })
+  ],
+  build: {
+    lib: {
+      entry: pkgJSON.source,
+      fileName: 'index',
+      formats: ['cjs', 'es']
+    },
+    rollupOptions: {
+      external: pkgJSON.peerDependencies
+    },
+    emptyOutDir: true,
+    outDir: 'dist',
+    minify: 'terser'
+  }
+});
+
+export { sharedConfig };
